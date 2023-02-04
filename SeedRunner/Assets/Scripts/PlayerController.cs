6811c6690eb86362e9046d36c6b2e6f7f7a50ae9
Anthony Ronda <anthonyronda@gmail.com>
2023-02-04 15:37:25 +0100

    fixed gravity and jump

--- SeedRunner/Assets/Scripts/PlayerController.cs
+++ SeedRunner/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : MonoBehaviour
 
     private float jumpTimer;
     private int jumpCounter;
-    private float groundPosY = 1.0f;
 
 
     // Start is called before the first frame update
@@ -25,7 +24,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // If Player is on the ground...
         if (isGrounded)
@@ -36,7 +35,7 @@ public class PlayerController : MonoBehaviour
             // ...set the position to the ground position
             Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
             // ... get rid of the downforce
-            rb.AddForce(new Vector2(0, -rb.velocity.y));
+            rb.AddForce(Vector2.up*rb.velocity.y);
         }
 
         // If W is pressed,
@@ -46,23 +45,29 @@ public class PlayerController : MonoBehaviour
         {
             // Add an upward force to the player
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            // Increment jump counter
+            jumpCounter += 1;
+            // Set the jump timer to now
+            jumpTimer = Time.fixedTime;
         }
     }
 
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-            //If player is colliding with the ground layer...
-            if (collision.tag == "Ground")
+        //If player is colliding with the ground layer...
+        if (collision.collider.tag == "Ground")
             // ...set the player to grounded
             isGrounded = true;
     }
-    private void OnTriggerExit2D(Collider2D collision)
+
+    private void OnCollisionExit2D(Collision2D collision)
     {
         // If player is no longer colliding with the ground layer...
-        if (collision.tag == "Ground")
+        if (collision.collider.tag == "Ground")
             // ...unset isGrounded
             isGrounded = false;
     }
+
 }
